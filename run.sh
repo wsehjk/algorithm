bcdd7ab98e8194a3d79211c43ef1fd1f6df2392e
wang shiwen <Wse1714401046@gmail.com>
2024-02-05 11:14:21 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -12,10 +12,10 @@ JAVAS=$(ls *.java)
 file=$1
 EXEC_PARAMS=($@)
 EXEC_PARAMS=${EXEC_PARAMS[@]:1:2}
-javac-algs4 $file 
-file=$(echo "${file%%.*}")
+javac-algs4 $file   # compile
+file=$(echo "${file%%.*}") # 获取文件名前缀
 
 echo -e "\033[33mbinary is $file\033[0m"
 echo -e "\033[33mparas is $EXEC_PARAMS\033[0m"
 
-java-algs4 $file $EXEC_PARAMS
+java-algs4 $file $EXEC_PARAMS # execute
