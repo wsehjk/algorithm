9380928346de10f50e71d5deb5be7621c75b9eb2
wang shiwen <Wse1714401046@gmail.com>
2024-02-11 23:14:17 +0800

    kmp

---
+++ cpp/string_search/kmp_nfa.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+class KMP {
+   private:
+    std::string pat_;
+    std::vector<int> next_;
+    void GetNext() {
+        int n = pat_.size();
+        for (int i = 1; i < n; ++i) {
+            int k = next_[i - 1];
+            while (k && pat_[k] != pat_[i]) {
+                k = next_[k - 1];
+            }
+            // if (pat_[k] == pat_[i]) {
+            //     k ++;
+            // }
+            k += pat_[k] == pat_[i];  // 避免 branch
+            next_[i] = k;
+        }
+    }
+
+   public:
+    KMP(const std::string& pat) : pat_(pat), next_(pat.size(), 0) { GetNext(); }
+    // 给定文本串，返回模式串在文本串中出现的位置
+    std::vector<int> Search(const std::string& txt) {
+        std::vector<int> pos;
+        int m = txt.size();
+        int c = 0;  //对于i, c
+                    //表示已经匹配的字符数，或者和txt[i]比较的模式串字符下标
+        for (int i = 0; i < m; ++i) {
+            while (c && pat_[c] != txt[i]) {
+                c = next_[c - 1];
+            }
+            // if (pat_[c] == txt[i]) {
+            //     c ++;
+            // }
+            c += pat_[c] == txt[i];
+            if (c == pat_.size()) {
+                pos.push_back(i - c + 1);
+            }
+        }
+        return pos;
+    }
+};
